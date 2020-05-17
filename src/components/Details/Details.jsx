cbfd76269bd6bbcf9db7aefb0d9ed9814b7ff84d
Tan Nguyen <tannguyen1024@gmail.com>
2020-05-16 20:39:19 -0500

    Removed ability to reload and maintain information due to issues

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -24,10 +24,10 @@ class Details extends Component {
             <div className="page">
                 <Button color="primary" variant="contained" onClick={this.home}>Return Home</Button> <Button color="secondary" variant="contained" onClick={this.handleClick}>Edit Movie</Button>
                 <tr>
-                    <td>NAME: {this.props.reduxState.clickedMovie2.map(info => <span>{info.title} </span>)}</td>
+                    <td>NAME: {this.props.reduxState.clickedMovie.title}</td>
                 </tr><br />
                 <tr>
-                    <td>DESCRIPTION: {this.props.reduxState.clickedMovie2.map(info => <span>{info.description} </span>)}</td>
+                    <td>DESCRIPTION: {this.props.reduxState.clickedMovie.description}</td>
                 </tr><br />
                 <tr>
                     <td>GENRE: {this.props.reduxState.genres.map(movies => <span>{movies.name} </span>)}</td>
